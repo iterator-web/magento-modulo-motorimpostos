0a72b8014f40b34ca42d1eac6fcc0c24f0e40f7e
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2014-12-11 10:52:41 -0200

    Commit referente ao issue #1
    Estruturação do Módulo

---
+++ app/code/local/Iterator/MotorImpostos/controllers/Adminhtml/NcmController.php
@@ -0,0 +1,92 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *                   MÓDULO DE MOTOR DE IMPOSTOS
+ * =================================================================
+ * Este produto foi desenvolvido para o Ecommerce Magento de forma a
+ * automatizar cálculos de impostos existentes em operações fiscais.
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará a conter diversos cálculos envolvendo documentos fiscais
+ * em operações de entradas e também de saídas de forma automática.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_MotorImpostos
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+class Iterator_MotorImpostos_Adminhtml_NcmController extends Mage_Adminhtml_Controller_Action {
+    
+    public function _construct() {
+        $helper = Mage::helper('motorimpostos');
+        if(!method_exists($helper, 'checkValidationMotorImpostos')) {
+            exit();
+        } else {
+            if(md5($_SERVER['HTTP_HOST'].'i_|*12*|_T'.$_SERVER['SERVER_ADDR']) != $helper->checkValidationMotorImpostos()) {
+                exit();
+            }
+        }
+    }
+    
+    public function indexAction() {
+        $this->_initAction()->renderLayout();
+    }
+    
+    protected function _initAction() {
+        $this->loadLayout()
+            ->_setActiveMenu('sales/iterator_motorimpostos/ncm')
+            ->_title($this->__('Sales'))->_title($this->__('Motor de Impostos'))
+            ->_addBreadcrumb($this->__('Sales'), $this->__('Sales'))
+            ->_addBreadcrumb($this->__('Motor de Impostos'), $this->__('Motor de Impostos'));
+         
+        return $this;
+    }
+    
+    protected function _isAllowed() {
+        return Mage::getSingleton('admin/session')->isAllowed('sales/motorimpostos');
+    }
+    
+    public function exportCsvAction() {
+        $fileName   = 'ncm_impostos.csv';
+        $content    = $this->getLayout()->createBlock('motorimpostos/adminhtml_motorimpostosncm_grid')
+            ->getCsv();
+ 
+        $this->_sendUploadResponse($fileName, $content);
+    }
+ 
+    public function exportXmlAction() {
+        $fileName   = 'ncm_impostos.xml';
+        $content    = $this->getLayout()->createBlock('motorimpostos/adminhtml_motorimpostosncm_grid')
+            ->getXml();
+ 
+        $this->_sendUploadResponse($fileName, $content);
+    }
+    
+    protected function _sendUploadResponse($fileName, $content, $contentType='application/octet-stream') {
+        $response = $this->getResponse();
+        $response->setHeader('HTTP/1.1 200 OK','');
+        $response->setHeader('Pragma', 'public', true);
+        $response->setHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0', true);
+        $response->setHeader('Content-Disposition', 'attachment; filename='.$fileName);
+        $response->setHeader('Last-Modified', date('r'));
+        $response->setHeader('Accept-Ranges', 'bytes');
+        $response->setHeader('Content-Length', strlen($content));
+        $response->setHeader('Content-type', $contentType);
+        $response->setBody($content);
+        $response->sendResponse();
+        die;
+    }
+}
+
+?>
---
+++ app/code/local/Iterator/MotorImpostos/sql/motorimpostos_setup/mysql4-install-1.0.0.php
@@ -0,0 +1,80 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *                   MÓDULO DE MOTOR DE IMPOSTOS
+ * =================================================================
+ * Este produto foi desenvolvido para o Ecommerce Magento de forma a
+ * automatizar cálculos de impostos existentes em operações fiscais.
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará a conter diversos cálculos envolvendo documentos fiscais
+ * em operações de entradas e também de saídas de forma automática.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_MotorImpostos
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+$installer = $this;
+$installer->startSetup();
+$installer->run("
+   CREATE  TABLE IF NOT EXISTS `{$installer->getTable('motorimpostos/ncm')}` (
+    `ncm_id` INT(12) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `ncm_codigo` INT(8) UNSIGNED NOT NULL,
+    `cfop_codigo` INT(4) UNSIGNED NULL,
+    `cfop_st_codigo` INT(4) UNSIGNED NULL,
+    `icms_cst` TINYINT(3) UNSIGNED NULL,
+    `icms_cst_st` TINYINT(3) UNSIGNED NULL,
+    `icms_origem` TINYINT(2) UNSIGNED NULL,
+    `icms_mod_bc` TINYINT(2) UNSIGNED NULL,
+    `icms_mod_bc_st` TINYINT(2) UNSIGNED NULL,
+    `ipi_cst` TINYINT(3) UNSIGNED NULL,
+    `pis_cofins_cst` TINYINT(3) UNSIGNED NULL,
+    `aliquota_ir` DOUBLE(7,4) UNSIGNED NULL,
+    `aliquota_csll` DOUBLE(7,4) UNSIGNED NULL,
+    `aliquota_pis` DOUBLE(7,4) UNSIGNED NULL,
+    `aliquota_cofins` DOUBLE(7,4) UNSIGNED NULL,
+    `aliquota_ipi` DOUBLE(7,4) UNSIGNED NULL,
+    `aliquota_ii` DOUBLE(7,4) UNSIGNED NULL,
+    `aliquota_iss` DOUBLE(7,4) UNSIGNED NULL,
+    `aliquota_interestadual` DOUBLE(7,4) UNSIGNED NULL,
+    PRIMARY KEY (`ncm_id`),
+    UNIQUE INDEX `ncm_codigo_UNIQUE` (`ncm_codigo` ASC))
+  ENGINE = InnoDB CHARSET=utf8;
+  
+  CREATE  TABLE IF NOT EXISTS `{$installer->getTable('motorimpostos/ncmuf')}` (
+    `ncm_uf_id` INT(12) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `ncm_id` INT(12) UNSIGNED NOT NULL,
+    `region_id` INT(10) UNSIGNED NOT NULL,
+    `aliquota_icms` DOUBLE(7,4) UNSIGNED NULL,
+    `mva_original` DOUBLE(7,4) UNSIGNED NULL,
+    PRIMARY KEY (`ncm_uf_id`),
+    INDEX `fk_iterator_motorimpostos_ncm_uf_iterator_motorimpostos_ncm_idx` (`ncm_id` ASC),
+    INDEX `fk_iterator_motorimpostos_ncm_uf_directory_country_region1_idx` (`region_id` ASC),
+    CONSTRAINT `fk_iterator_motorimpostos_ncm_uf_iterator_motorimpostos_ncm`
+      FOREIGN KEY (`ncm_id`)
+      REFERENCES `{$installer->getTable('motorimpostos/ncm')}` (`ncm_id`)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+    CONSTRAINT `fk_iterator_motorimpostos_ncm_uf_directory_country_region1`
+      FOREIGN KEY (`region_id`)
+      REFERENCES `{$installer->getTable('directory/country_region')}` (`region_id`)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE)
+  ENGINE = InnoDB CHARSET=utf8;
+  ");
+
+$installer->endSetup();
+
+?>
