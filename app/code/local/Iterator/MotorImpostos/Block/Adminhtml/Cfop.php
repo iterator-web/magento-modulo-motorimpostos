74220186186854f6eb3abc216c1136d7ac51af9b
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2014-12-16 12:26:28 -0200

    Commit referente ao issue #2
    Criar Grid de gerenciamento

---
+++ app/code/local/Iterator/MotorImpostos/Block/Adminhtml/Cfop.php
@@ -0,0 +1,41 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *                   MÓDULO DE MOTOR DE IMPOSTOS
+ * =================================================================
+ * Este produto foi desenvolvido para o Ecommerce Magento de forma a
+ * automatizar cálculos de impostos existentes em operações fiscais.
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará a conter diversos cálculos envolvendo documentos fiscais
+ * em operações de entradas e também de saídas de forma automática.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_MotorImpostos
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+class Iterator_MotorImpostos_Block_Adminhtml_Cfop extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+    public function __construct()
+    {
+        $this->_blockGroup = 'motorimpostos';
+        $this->_controller = 'adminhtml_cfop';
+        $this->_headerText = $this->__('Gerenciar CFOP');       
+         
+        parent::__construct();
+        $this->_updateButton('add', 'label', $this->__('Criar CFOP'));
+    }
+}
+?>
