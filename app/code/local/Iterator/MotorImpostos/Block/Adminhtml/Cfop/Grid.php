6417a1950d81266f00ab94ba86c1fc2b44312f6d
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2014-12-17 10:30:45 -0200

    Commit complementar ao issue #2
    Criar Grid de gerenciamento

--- app/code/local/Iterator/MotorImpostos/Block/Adminhtml/Cfop/Grid.php
+++ app/code/local/Iterator/MotorImpostos/Block/Adminhtml/Cfop/Grid.php
@@ -129,6 +129,21 @@ class Iterator_MotorImpostos_Block_Adminhtml_Cfop_Grid extends Mage_Adminhtml_Bl
          
         return parent::_prepareColumns();
     }
+    
+    public function getRowUrl($row) {
+        return $this->getUrl('*/*/edit', array('id' => $row->getId()));
+    }
+    
+    protected function _prepareMassaction() {
+        $this->setMassactionIdField('cfop_id');
+        $this->getMassactionBlock()->setFormFieldName('cfop_id');
+        $this->getMassactionBlock()->addItem('delete', array(
+        'label'=> Mage::helper('motorimpostos')->__('Delete'),
+        'url'  => $this->getUrl('*/*/massDelete', array('' => '')),
+        'confirm' => Mage::helper('motorimpostos')->__('Are you sure?')
+        ));
+        return $this;
+    }
 }
 
 ?>
